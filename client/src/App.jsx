9ead0edc2a49f2ce7e024b871f967d34658b0fd1
d0ndada <pintoavila09@gmail.com>
2023-06-05 11:20:15 +0200

    scroll arrow & footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
 import axios from "axios";
 
 function App() {
   const [blockchain, setBlockchain] = useState([]);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [showScrollArrow, setShowScrollArrow] = useState(false); // state to control arrow display
+  const blockContainerRef = useRef(null);
 
   const url = "http://localhost:5000/api/1/blocks";
 
@@ -13,6 +15,7 @@ function App() {
       const { data } = await axios.get(url);
       console.log(data.data);
       setBlockchain(data.data);
+      handleScroll();
     } catch (error) {
       if (error.response && error.response.status === 400) {
         setErrorMessage(error.response.data.error);
@@ -39,45 +42,85 @@ function App() {
       getBlockchain();
       e.target.reset();
     } catch (error) {
-      if (error.response && error.response.data && error.response.data.error) {
+      if (error.response) {
         setErrorMessage(error.response.data.error);
-        console.log(errorMessage);
-        console.log(error.response.data.error.status);
+        // console.log(errorMessage);
+        // console.log(error.response.data.error.status);
       } else {
         setErrorMessage("An unexpected error occurred.");
       }
     }
   };
+  const handleScroll = () => {
+    if (blockContainerRef.current) {
+      const { scrollHeight, clientHeight, scrollTop } =
+        blockContainerRef.current;
+      setShowScrollArrow(scrollTop < clientHeight + scrollHeight);
+    }
+  };
 
+  useEffect(() => {
+    if (blockContainerRef.current) {
+      blockContainerRef.current.addEventListener("scroll", handleScroll);
+    }
+
+    // Clean up - remove the event listener
+    return () => {
+      if (blockContainerRef.current) {
+        blockContainerRef.current.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, [blockchain]);
   return (
     <div className="App">
-      <h3 className="title">Blockchain Client</h3>
-      <div className="form-container">
-        <form onSubmit={handleSubmit} className="addBlock">
-          <div className="form__group field">
-            <input
-              type="text"
-              className="form__field"
-              placeholder="add block"
-              name="data"
-              id="name"
-              onChange={handleChange}
-            />
-            <label htmlFor="name" className="form__label">
-              Add block
-            </label>
-            <button className="submit-btn">Add</button>
-          </div>
-        </form>
-        {errorMessage && <p className="error">{errorMessage}</p>}
-      </div>
-      <ul>
-        {blockchain.map((block) => (
-          <li className="block" key={block.hash}>
-            <p className="block-text">{block.data}</p>
-          </li>
-        ))}
-      </ul>
+      <header className="header">
+        <h3 className="title">Blockchain Client</h3>
+      </header>
+      <main className="main">
+        <div className="form-container">
+          <form onSubmit={handleSubmit} className="addBlock">
+            <div className="form__group field">
+              <input
+                type="text"
+                className="form__field"
+                placeholder="add block"
+                name="data"
+                id="name"
+                onChange={handleChange}
+              />
+              <label htmlFor="name" className="form__label">
+                Add block
+              </label>
+              <button className="submit-btn">Add</button>
+            </div>
+          </form>
+          {errorMessage && <p className="error">{errorMessage}</p>}
+        </div>
+        <div className="block-container" ref={blockContainerRef}>
+          <ul>
+            {blockchain.map((block) => (
+              <li className="block" key={block.hash}>
+                <p className="block-text">{block.data}</p>
+              </li>
+            ))}
+          </ul>
+          {showScrollArrow && (
+            <div id="scrollArrow" className="scroll-arrow">
+              ↓
+            </div>
+          )}
+        </div>
+      </main>
+      <footer className="footer">
+        <p className="footer-text">&copy; 2023 d0ndada </p>
+        <a
+          className="link"
+          href="https://github.com/d0ndada"
+          target="_blank"
+          rel="noopener noreferrer">
+          <img className="GithubLogo" alt="github" src="/github-mark.png" />
+        </a>
+      </footer>
     </div>
   );
 }
