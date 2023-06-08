c03685e080d2316712735982beb7903a21ee64f3
d0ndada <pintoavila09@gmail.com>
2023-06-08 13:46:48 +0200

    added const for fucntionality

---
+++ backend/utilities/config.js
@@ -0,0 +1,16 @@
+const createHash = require("./hash");
+
+const MINE_RATE = 1000;
+const INITIAL_DIFFICULTY = 3;
+const INITIAL_BALANCE = 500;
+
+const GENESIS_DATA = {
+  timestamp: 1,
+  data: "dummy data",
+  hash: createHash(),
+  difficulty: INITIAL_DIFFICULTY,
+  nonce: 0,
+  lastHash: "######",
+};
+
+module.exports = GENESIS_DATA;
