a1f8dae0cde447018e36bb477c2a7e0ded966280
d0ndada <pintoavila09@gmail.com>
2023-06-09 12:15:39 +0200

    synchrinizing handel transactionPool

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
+const cors = require("cors");
 const express = require("express");
 const axios = require("axios");
 const Blockchain = require("./blockchain/Blockchain");
 const Blocks = require("./routes/blockchain-routes");
-const cors = require("cors");
+const TransactionPool = require("./Wallet/TransactionPool");
+const Wallet = require("./Wallet/Wallet");
+const Broker = require("./messageBroker/Broker");
 
 const app = express();
-
 const blockchain = new Blockchain();
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
+const messageBroker = new Broker(blockchain, transactionPool);
 
 const DEFAULT_PORT = 5000;
 const ROOT_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -14,9 +19,17 @@ const ROOT_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 //SYNC data at startup
 const syncData = async () => {
   try {
-    const url = `${ROOT_ADDRESS}/api/1/blocks`;
-    const { data } = await axios.get(url);
+    const blockchainUrl = `${ROOT_ADDRESS}/api/1/blocks`;
+    const transactionMapUrl = `${ROOT_ADDRESS}/api/1/Transaction`;
+    let result = await axios.get(blockchainUrl);
+
     blockchain.replaceChain(data);
+    console.log("Synchronizing at startup");
+
+    result = await axios.get(transactionMapUrl);
+    const transactions = result.data;
+    console.log("Synchronizing at startup");
+    transactionPool.replacePool(transactions);
   } catch (err) {
     console.log("Error", err);
   }
