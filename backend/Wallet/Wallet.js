5213b18463d8c5da889dee7b74b72ea5abceeead
d0ndada <pintoavila09@gmail.com>
2023-06-09 09:23:19 +0200

    added function and test works

--- backend/Wallet/Wallet.js
+++ backend/Wallet/Wallet.js
@@ -1,12 +1,15 @@
 const { ec } = require("../utilities");
 const { INITIAL_BALANCE } = require("../utilities/config");
-
+const crypto = require("../utilities/hash");
 class Wallet {
   constructor() {
     this.balance = INITIAL_BALANCE;
 
     this.keyPair = ec.genKeyPair();
     this.publicKey = this.keyPair.getPublic().encode("hex");
+  }
+  sign(data) {
+    return this.keyPair.sign(crypto(data));
   }
 }
 
--- backend/Wallet/wallet.test.js
+++ backend/Wallet/wallet.test.js
@@ -27,5 +27,26 @@ describe("Wallet", () => {
         })
       ).toBe(true);
     });
+    describe("create transactions", () => {
+      describe("and the amount that exceeds the funs", () => {
+        it("throws an error", () => {
+          expect(() =>
+            wallet.createTransaction({ amount: 10000, recipient: "Tony" })
+          ).toThrow("Not enough funds");
+        });
+      });
+      describe("and the amount is valid", () => {
+        let transaction, amount, recipient;
+
+        beforeEach(() => {
+          amount = 100;
+          recipient = "Danny";
+          transaction = wallet.createTransaction({ amount, recipient });
+        });
+        it("it creates a new instance with the wallet ", () => {
+          expect(transaction instanceof Transaction).toBe(true);
+        });
+      });
+    });
   });
 });
