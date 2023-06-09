a9d6d6826a53fdb3532855604d1bec906687bc0e
d0ndada <pintoavila09@gmail.com>
2023-06-09 11:51:21 +0200

    transactionPool testing

---
+++ backend/Wallet/transaction-pool.test.js
@@ -0,0 +1,32 @@
+const Transaction = require("./Transaction");
+const Wallet = require("./Wallet");
+
+describe("TransactionPool", () => {
+  let transactionPool, transaction;
+
+  beforeEach(() => {
+    transactionPool = new TransactionPool();
+    sender = new Wallet();
+    transaction = new Transaction({
+      sender,
+      recipient: "test-recipient",
+      amount: 100,
+    });
+  });
+  describe("the addTransaction method", () => {
+    it("should add a new transaction to the transactionpool", () => {
+      transactionPool.addTransaction(transaction);
+      expect(transactionPool.transactionMap[transaction.id]).toBe(transaction);
+    });
+  });
+  describe("the transactionExist method", () => {
+    it("should return an existing transaction based on the input address", () => {
+      transactionPool.addTransaction(transaction);
+      expect(
+        transactionPool.transactionExist({
+          address: sender.publicKey,
+        })
+      ).toBe(transaction);
+    });
+  });
+});
